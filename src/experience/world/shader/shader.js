040092712d0e58822f9909231590dc1cb00811df
Tal Hayut <talha@accessibe.com>
2022-07-10 13:24:19 +0300

    fix: audio set on mobile

--- src/experience/world/shader/shader.js
+++ src/experience/world/shader/shader.js
@@ -30,6 +30,10 @@ export default class Shader {
 
     window.addEventListener('touchstart', (e) => {
       this.animate();
+      if (!audioSet) {
+        this.setAudio();
+        audioSet = true;
+      }
     });
 
     this.setGeometry();
